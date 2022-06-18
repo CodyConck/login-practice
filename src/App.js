753e2a7910abaf76cfde232dc8f202cb08b4482a
CodyConck <codyconck@gmail.com>
2022-06-18 13:14:25 -0500

    installed reactboostrap and added form to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Home from './Pages/home'
 
 function App() {
   return (
     <div>
-      <p>Clean React App</p>
+      <Home />
     </div>
   )
 }
